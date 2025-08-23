f5d4b57b25367d61f3d1112b36418a11365e89bf
Poldverg <1polinapercel1@gmail.com>
2025-08-23 18:28:44 +0300

    fixing sidebars

--- frontend/src/components/SidebarLeft.jsx
+++ frontend/src/components/SidebarLeft.jsx
@@ -23,23 +23,16 @@ const blogData = [
 
 const SidebarTop = () => {
   return (
-    <aside className=" flex 
-  flex-col 
-  justify-center 
-  h-full 
-  px-[1.2rem] 
-  py-4 
-  bg-secondary 
-  text-primary 
-  border-l-[3px] 
-  border-t-[3px] 
-  border-primary 
-  basis-[23%] 
-  flex-[0_0_20%]">
+    <aside className=" flex flex-col justify-start
+    h-[48%]
+    px-[1.2rem] py-4
+    bg-secondary text-primary
+    border-t-[3px] border-r-[3px] border-primary
+    flex-[0_0_23%]">
       <img
         src={assets.blogLogo}
         alt="Блог логотип"
-        className="w-[clamp(100px,20vw,200px)] pb-4 px-4 mx-auto"
+        className="w-full pb-4 px-4 mx-auto"
       />
 
       <div className="flex flex-col gap-3">
@@ -47,14 +40,14 @@ const SidebarTop = () => {
           <BlogCard key={index} {...blog} />
         ))}
 
-      <Link
-  to="/Blog"
-  className="flex items-center justify-center gap-4 text-[clamp(16px,1.4vw,20px)] font-literata font-medium text-primary hover:text-accent transition-colors"
->
-  <span className="flex-1 h-[3px] bg-current transition-all duration-300"></span>
-  Еще
-  <span className="flex-1 h-[3px] bg-current transition-all duration-300"></span>
-</Link>
+        <Link
+          to="/Blog"
+          className="flex items-center justify-center gap-4 text-[clamp(16px,1.4vw,20px)] font-literata font-medium text-primary hover:text-accent transition-colors"
+        >
+          <span className="flex-1 h-[3px] bg-current transition-all duration-300"></span>
+          Еще
+          <span className="flex-1 h-[3px] bg-current transition-all duration-300"></span>
+        </Link>
       </div>
     </aside>
   );
