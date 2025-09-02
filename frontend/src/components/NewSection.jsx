027e274a5d156c4770f59fcb4b4419b6032f01db
Poldverg <1polinapercel1@gmail.com>
2025-09-02 15:46:09 +0300

    Latest products actually display latest ones

--- frontend/src/components/NewSection.jsx
+++ frontend/src/components/NewSection.jsx
@@ -4,8 +4,9 @@ import ProductGrid from "./ProductGrid"
 
 const NewSection = () => {
   const { products } = useContext(ShopContext)
+  const latestProducts = products.slice(-9)
 
-  return <ProductGrid title="Новинки" products={products} />
+  return <ProductGrid title="Новинки" products={latestProducts} />
 }
 
 export default NewSection
